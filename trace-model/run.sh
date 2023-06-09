ffac931630ab5e45ecba900d59f88b1425ab486a
Ubuntu <ubuntu@ip-172-31-42-43.ec2.internal>
2023-06-09 04:47:13 +0000

    Updated repo

--- trace-model/run.sh
+++ trace-model/run.sh
@@ -3,4 +3,4 @@
 source docker.properties
 
 # Edit for using multiple chips
-docker run -t -d --name ${docker_container_name} -v /home/ubuntu/fastapi-inf-blog/trace-model:/trace-model --device /dev/neuron0  ${registry}/${docker_image_name} 
+docker run -t -d --name ${docker_container_name} -v /home/ubuntu/best-practices-for-fastapi-on-inferentia/trace-model:/trace-model --device /dev/neuron0  ${registry}/${docker_image_name} 
