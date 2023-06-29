5005b02d190fd01dc556c66ea325164b4bd26574
pronoc <pronoc@amazon.com>
2023-06-29 12:05:11 -0700

    Removed logger and duplicate imports

--- fast-api/fastapi-server.py
+++ fast-api/fastapi-server.py
@@ -1,10 +1,10 @@
 from typing import Optional
-from fastapi import FastAPI,logger,responses
+from fastapi import FastAPI, responses
 from configparser import ConfigParser
-import torch, os, logging
+import torch
+import logging
 import importlib
 import os
-
 
 global device
 global processor
@@ -12,7 +12,6 @@ global models
 global tokenizers
 global logger
 global default_question, default_context
-
 
 chip_type = os.environ.get("CHIP_TYPE", "inf1")
 if chip_type == "inf1":
@@ -40,7 +39,6 @@ model_class_name = 'AutoModelForQuestionAnswering'
 transformers = importlib.import_module("transformers")
 tokenizer_class = getattr(transformers, tokenizer_class_name)
 
-
 default_question = "What does the little engine say"
 default_context = """In the childrens story about the little engine a small locomotive is pulling a large load up a mountain.
     Since the load is heavy and the engine is small it is not sure whether it will be able to do the job. This is a story 
@@ -48,64 +46,70 @@ default_context = """In the childrens story about the little engine a small loco
     pulling the heavy load all the way to the top of the mountain. On the way down it says: I thought I could."""
 
 env_var = os.getenv('NEURON_RT_VISIBLE_CORES')
-neuron_core=int(env_var[0])
+neuron_core = int(env_var[0])
 
 # FastAPI server
 app = FastAPI()
+
 
 # Server healthcheck
 @app.get("/")
 async def read_root():
     return {"Status": "Healthy"}
 
+
 prediction_api_name = f'predictions_neuron_core_{neuron_core}'
 postprocess = True
 quiet = False
 
+
 # Model inference API endpoint
 @app.get("/{prediction_api_name}/{model_id}")
 async def infer(model_id, seq_0: Optional[str] = default_question, seq_1: Optional[str] = default_context):
-    question=seq_0
-    context=seq_1
-    status=200
+    question = seq_0
+    context = seq_1
+    status = 200
     if model_id in models.keys():
 
         if not quiet:
             logger.warning(f"\nQuestion: {question}\n")
 
-        tokenizer=tokenizers[model_id]
-        encoded_input = tokenizer.encode_plus(question, context, return_tensors='pt', max_length=128, padding='max_length', truncation=True)
-        model=models[model_id]
-        model_input = (encoded_input['input_ids'],  encoded_input['attention_mask'])
-        output=model(*model_input) # This is specific to Inferentia
+        tokenizer = tokenizers[model_id]
+        encoded_input = tokenizer.encode_plus(question, context, return_tensors='pt', max_length=128,
+                                              padding='max_length', truncation=True)
+        model = models[model_id]
+        model_input = (encoded_input['input_ids'], encoded_input['attention_mask'])
+        output = model(*model_input)  # This is specific to Inferentia
         answer_text = str(output[0])
 
         if postprocess:
             answer_start = torch.argmax(output[0])
-            answer_end = torch.argmax(output[1])+1
+            answer_end = torch.argmax(output[1]) + 1
             if (answer_end > answer_start):
-                answer_text = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(encoded_input["input_ids"][0][answer_start:answer_end]))
+                answer_text = tokenizer.convert_tokens_to_string(
+                    tokenizer.convert_ids_to_tokens(encoded_input["input_ids"][0][answer_start:answer_end]))
             else:
-                answer_text = tokenizer.convert_tokens_to_string(tokenizer.convert_ids_to_tokens(encoded_input["input_ids"][0][answer_start:]))
+                answer_text = tokenizer.convert_tokens_to_string(
+                    tokenizer.convert_ids_to_tokens(encoded_input["input_ids"][0][answer_start:]))
         if not quiet:
             logger.warning("\nAnswer: ")
             logger.warning(answer_text)
     else:
-        status=404
-        answer_text = f"Model {model_id} does not exist. Try a model name up to model{num_models-1}"
+        status = 404
+        answer_text = f"Model {model_id} does not exist. Try a model name up to model{num_models - 1}"
         if not quiet:
             logger.warning(answer_text)
     return responses.JSONResponse(status_code=status, content={"detail": answer_text})
 
 
-tokenizers={}
-models={}
+tokenizers = {}
+models = {}
 # LOAD Models
-print('Num of models to be loaded = '+str(num_models_per_server))
+print('Num of models to be loaded = ' + str(num_models_per_server))
 
 for i in range(num_models_per_server):
     model_id = f'model_{i}'
     # Load the compiled models
     print(f'Loading Model {i} ....')
     models[model_id] = torch.jit.load(f'/app/server/traced-models/{compiled_model}')
-    tokenizers[model_id]=tokenizer_class.from_pretrained(model_name)
+    tokenizers[model_id] = tokenizer_class.from_pretrained(model_name)
