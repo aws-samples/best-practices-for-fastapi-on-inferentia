b17fa25e92b3f3bfcba84f181cb1273f5ddd015f
Pronoy Chopra <pronoyc@gmail.com>
2023-06-29 08:24:20 +0000

    Support for chip type and human readability

--- fast-api/run.sh
+++ fast-api/run.sh
@@ -6,7 +6,6 @@ source docker.properties
 num_models=$(($num_cores * $num_models_per_server))
 
 
-
 for i in $(seq 1 $num_cores); do
     echo "Running Container on Neuron Core $i"
 
@@ -15,7 +14,11 @@ for i in $(seq 1 $num_cores); do
 
     device_num=$(($core_num/4))
 
-    docker run -t -d --name ${docker_container_name}'-nc'${core_num} -v ${path_to_traced_models}:/app/server/traced-models --env NEURON_RT_VISIBLE_CORES="${core_num}-${core_num}" -p ${port_num}:8080 --device=/dev/neuron${device_num}  ${registry}/${docker_image_name}
+    docker run -t -d \
+	    --name ${docker_container_name}'-nc'${core_num} \
+	    --env NEURON_RT_VISIBLE_CORES="${core_num}-${core_num}" \
+	    --env CHIP_TYPE=${CHIP_TYPE} \
+	    -p ${port_num}:8080 --device=/dev/neuron${device_num} ${registry}/${docker_image_name}
 done
 
 
