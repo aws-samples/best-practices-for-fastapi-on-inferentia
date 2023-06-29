c411f29cae65b3c31200fe3b3ad144173a12642f
pronoc <pronoc@amazon.com>
2023-06-29 13:07:25 -0700

    removed credential fetch

--- trace-model/build.sh
+++ trace-model/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 source docker.properties
 
-echo "Getting ECR credentials to fetch torch-neuron and torch-neuronx deep learning container images for region ${AWS_REGION}"
-aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin 763104351884.dkr.ecr.${AWS_REGION}.amazonaws.com
-
 DOCKER_BUILDKIT=1 docker build -t ${registry}/${docker_image_name} --build-arg BASE_IMAGE=$BASE_IMAGE -f Dockerfile ..
